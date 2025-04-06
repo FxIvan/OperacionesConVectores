b1d7631e7f9d3c98ae2bbbf81bbeb8aee875948f
Ivan Lopez <ialmendra@infobae.com>
2025-04-06 13:45:30 -0300

    Agregar archivos de proyecto.

---
+++ OperacionesConVectores/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace OperacionesConVectores
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.lstText = new System.Windows.Forms.ListBox();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.btnCopiar = new System.Windows.Forms.Button();
+            this.btnLimites = new System.Windows.Forms.Button();
+            this.btnBusquedaBinaria = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.txtNombreUno = new System.Windows.Forms.TextBox();
+            this.txtNombreDos = new System.Windows.Forms.TextBox();
+            this.txtNombreTres = new System.Windows.Forms.TextBox();
+            this.txtNombreCuatro = new System.Windows.Forms.TextBox();
+            this.txtNombreCinco = new System.Windows.Forms.TextBox();
+            this.txtNombreSeis = new System.Windows.Forms.TextBox();
+            this.txtNombreSiete = new System.Windows.Forms.TextBox();
+            this.txtNombreOcho = new System.Windows.Forms.TextBox();
+            this.txtNombreNueve = new System.Windows.Forms.TextBox();
+            this.txtNombreDiez = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtValueSearchBinary = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(88, 12);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(218, 33);
+            this.btnCargar.TabIndex = 0;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // lstText
+            // 
+            this.lstText.FormattingEnabled = true;
+            this.lstText.Location = new System.Drawing.Point(31, 67);
+            this.lstText.Name = "lstText";
+            this.lstText.Size = new System.Drawing.Size(330, 238);
+            this.lstText.TabIndex = 1;
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(31, 312);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(148, 33);
+            this.btnOrdenar.TabIndex = 2;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // btnCopiar
+            // 
+            this.btnCopiar.Location = new System.Drawing.Point(213, 312);
+            this.btnCopiar.Name = "btnCopiar";
+            this.btnCopiar.Size = new System.Drawing.Size(148, 33);
+            this.btnCopiar.TabIndex = 3;
+            this.btnCopiar.Text = "Copiar";
+            this.btnCopiar.UseVisualStyleBackColor = true;
+            this.btnCopiar.Click += new System.EventHandler(this.btnCopiar_Click);
+            // 
+            // btnLimites
+            // 
+            this.btnLimites.Location = new System.Drawing.Point(31, 351);
+            this.btnLimites.Name = "btnLimites";
+            this.btnLimites.Size = new System.Drawing.Size(148, 33);
+            this.btnLimites.TabIndex = 4;
+            this.btnLimites.Text = "Limites";
+            this.btnLimites.UseVisualStyleBackColor = true;
+            this.btnLimites.Click += new System.EventHandler(this.btnLimites_Click);
+            // 
+            // btnBusquedaBinaria
+            // 
+            this.btnBusquedaBinaria.Location = new System.Drawing.Point(213, 351);
+            this.btnBusquedaBinaria.Name = "btnBusquedaBinaria";
+            this.btnBusquedaBinaria.Size = new System.Drawing.Size(148, 33);
+            this.btnBusquedaBinaria.TabIndex = 5;
+            this.btnBusquedaBinaria.Text = "Busq. Binaria";
+            this.btnBusquedaBinaria.UseVisualStyleBackColor = true;
+            this.btnBusquedaBinaria.Click += new System.EventHandler(this.btnBusquedaBinaria_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(121, 396);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(148, 33);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // txtNombreUno
+            // 
+            this.txtNombreUno.Location = new System.Drawing.Point(384, 67);
+            this.txtNombreUno.Name = "txtNombreUno";
+            this.txtNombreUno.Size = new System.Drawing.Size(239, 20);
+            this.txtNombreUno.TabIndex = 7;
+            // 
+            // txtNombreDos
+            // 
+            this.txtNombreDos.Location = new System.Drawing.Point(384, 93);
+            this.txtNombreDos.Name = "txtNombreDos";
+            this.txtNombreDos.Size = new System.Drawing.Size(239, 20);
+            this.txtNombreDos.TabIndex = 8;
+            // 
+            // txtNombreTres
+            // 
+            this.txtNombreTres.Location = new System.Drawing.Point(384, 119);
+            this.txtNombreTres.Name = "txtNombreTres";
+            this.txtNombreTres.Size = new System.Drawing.Size(239, 20);
+            this.txtNombreTres.TabIndex = 9;
+            // 
+            // txtNombreCuatro
+            // 
+            this.txtNombreCuatro.Location = new System.Drawing.Point(384, 145);
+            this.txtNombreCuatro.Name = "txtNombreCuatro";
+            this.txtNombreCuatro.Size = new System.Drawing.Size(239, 20);
+            this.txtNombreCuatro.TabIndex = 10;
+            this.txtNombreCuatro.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtNombreCinco
+            // 
+            this.txtNombreCinco.Location = new System.Drawing.Point(384, 171);
+            this.txtNombreCinco.Name = "txtNombreCinco";
+            this.txtNombreCinco.Size = new System.Drawing.Size(239, 20);
+            this.txtNombreCinco.TabIndex = 11;
+            // 
+            // txtNombreSeis
+            // 
+            this.txtNombreSeis.Location = new System.Drawing.Point(384, 197);
+            this.txtNombreSeis.Name = "txtNombreSeis";
+            this.txtNombreSeis.Size = new System.Drawing.Size(239, 20);
+            this.txtNombreSeis.TabIndex = 12;
+            // 
+            // txtNombreSiete
+            // 
+            this.txtNombreSiete.Location = new System.Drawing.Point(384, 223);
+            this.txtNombreSiete.Name = "txtNombreSiete";
+            this.txtNombreSiete.Size = new System.Drawing.Size(239, 20);
+            this.txtNombreSiete.TabIndex = 13;
+            // 
+            // txtNombreOcho
+            // 
+            this.txtNombreOcho.Location = new System.Drawing.Point(384, 249);
+            this.txtNombreOcho.Name = "txtNombreOcho";
+            this.txtNombreOcho.Size = new System.Drawing.Size(239, 20);
+            this.txtNombreOcho.TabIndex = 14;
+            // 
+            // txtNombreNueve
+            // 
+            this.txtNombreNueve.Location = new System.Drawing.Point(384, 275);
+            this.txtNombreNueve.Name = "txtNombreNueve";
+            this.txtNombreNueve.Size = new System.Drawing.Size(239, 20);
+            this.txtNombreNueve.TabIndex = 15;
+            // 
+            // txtNombreDiez
+            // 
+            this.txtNombreDiez.Location = new System.Drawing.Point(384, 301);
+            this.txtNombreDiez.Name = "txtNombreDiez";
+            this.txtNombreDiez.Size = new System.Drawing.Size(239, 20);
+            this.txtNombreDiez.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(381, 341);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Numero que se debe buscar:";
+            // 
+            // txtValueSearchBinary
+            // 
+            this.txtValueSearchBinary.Location = new System.Drawing.Point(384, 358);
+            this.txtValueSearchBinary.Name = "txtValueSearchBinary";
+            this.txtValueSearchBinary.Size = new System.Drawing.Size(239, 20);
+            this.txtValueSearchBinary.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 443);
+            this.Controls.Add(this.txtValueSearchBinary);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombreDiez);
+            this.Controls.Add(this.txtNombreNueve);
+            this.Controls.Add(this.txtNombreOcho);
+            this.Controls.Add(this.txtNombreSiete);
+            this.Controls.Add(this.txtNombreSeis);
+            this.Controls.Add(this.txtNombreCinco);
+            this.Controls.Add(this.txtNombreCuatro);
+            this.Controls.Add(this.txtNombreTres);
+            this.Controls.Add(this.txtNombreDos);
+            this.Controls.Add(this.txtNombreUno);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnBusquedaBinaria);
+            this.Controls.Add(this.btnLimites);
+            this.Controls.Add(this.btnCopiar);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.lstText);
+            this.Controls.Add(this.btnCargar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.ListBox lstText;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.Button btnCopiar;
+        private System.Windows.Forms.Button btnLimites;
+        private System.Windows.Forms.Button btnBusquedaBinaria;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox txtNombreUno;
+        private System.Windows.Forms.TextBox txtNombreDos;
+        private System.Windows.Forms.TextBox txtNombreTres;
+        private System.Windows.Forms.TextBox txtNombreCuatro;
+        private System.Windows.Forms.TextBox txtNombreCinco;
+        private System.Windows.Forms.TextBox txtNombreSeis;
+        private System.Windows.Forms.TextBox txtNombreSiete;
+        private System.Windows.Forms.TextBox txtNombreOcho;
+        private System.Windows.Forms.TextBox txtNombreNueve;
+        private System.Windows.Forms.TextBox txtNombreDiez;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtValueSearchBinary;
+    }
+}
+
---
+++ OperacionesConVectores/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace OperacionesConVectores
+{
+    public partial class Form1 : Form
+    {
+        int[] vector;
+        int i;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            lstText.Items.Clear();
+            vector = new int[10];
+            
+            for (i = 0; i < vector.Length; i++) {
+                if (i == 0 && txtNombreUno.Text.Length != 0)
+                {
+                    vector[i] = Int32.Parse(txtNombreUno.Text);
+                } else if (i == 1 && txtNombreDos.Text.Length != 0)
+                {
+                    vector[i] = Int32.Parse(txtNombreDos.Text);
+                }else if (i == 2 && txtNombreTres.Text.Length != 0)
+                {
+                    vector[i] = Int32.Parse(txtNombreTres.Text);
+                }else if (i == 3 && txtNombreCuatro.Text.Length != 0)
+                {
+                    vector[i] = Int32.Parse(txtNombreCuatro.Text);
+                }else if (i == 4 && txtNombreCinco.Text.Length != 0)
+                {
+                    vector[i] = Int32.Parse(txtNombreCinco.Text);
+                }else if (i == 5 && txtNombreSeis.Text.Length != 0)
+                {
+                    vector[i] = Int32.Parse(txtNombreSeis.Text);
+                }else if (i == 6 && txtNombreSiete.Text.Length != 0)
+                {
+                    vector[i] = Int32.Parse(txtNombreSiete.Text);
+                }else if (i == 7 && txtNombreOcho.Text.Length != 0)
+                {
+                    vector[i] = Int32.Parse(txtNombreOcho.Text);
+                }else if (i == 8 && txtNombreNueve.Text.Length != 0)
+                {
+                    vector[i] = Int32.Parse(txtNombreNueve.Text);
+                }else if (i == 9 && txtNombreDiez.Text.Length != 0)
+                {
+                    vector[i] = Int32.Parse(txtNombreDiez.Text);
+                }
+
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                lstText.Items.Add("En la posición : " + i + " el valor es: " +
+               vector[i]);
+            }
+            txtNombreUno.Text = string.Empty;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            lstText.Items.Clear();
+            this.Close();
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            lstText.Items.Clear();
+            Array.Sort(vector);
+            for (int i = 0; i < 10; i++)
+            {
+                lstText.Items.Add("El valor es: " + vector[i] + " en la posición : " + i);
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCopiar_Click(object sender, EventArgs e)
+        {
+            lstText.Items.Clear();
+            int[] vectorDestino = new int[20];
+            Array.Copy(vector, 0, vectorDestino, 0, 10);
+            for (int i = 0; i < 10; i++)
+            {
+                lstText.Items.Add(vectorDestino[i] + " en la posición " + i);
+            }
+        }
+
+        /*
+         NOTA: El algoritmo de búsqueda binaria es un excelente método para buscar datos dentro de una 
+        estructura (generalmente un arreglo unidimensional). Se le da el nombre de búsqueda binaria por 
+        que el algoritmo divide en dos el array, aludiendo al concepto de bit, el cual puede tener dos 
+        estados.
+        La única condición para usar este algoritmo es que los datos dentro del array estén ordenados de 
+        menor a mayor.
+         */
+
+        private void btnBusquedaBinaria_Click(object sender, EventArgs e)
+        {
+            int posicion;
+            int bynaryValue = Int32.Parse(txtValueSearchBinary.Text);
+            posicion = Array.BinarySearch(vector, bynaryValue);
+            lstText.Items.Clear();
+            lstText.Items.Add("En la posicion " + posicion + " se encuentra el número " + bynaryValue);
+
+        }
+
+        private void btnLimites_Click(object sender, EventArgs e)
+        {
+            int superior;
+            int inferior;
+            lstText.Items.Clear();
+            // https://learn.microsoft.com/es-es/dotnet/api/system.array.getupperbound?view=net-8.0
+            // GetUpperBound nos dice Índice máximo y minimo del array(GetUpperBound y GetLowerBound)
+            superior = vector.GetUpperBound(0);
+            inferior = vector.GetLowerBound(0);
+            lstText.Items.Add("El límite inferior es " + inferior + " y el superior es " + superior);
+        }
+    }
+}
